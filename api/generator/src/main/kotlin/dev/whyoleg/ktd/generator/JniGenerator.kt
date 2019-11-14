9c4f6a30214ef5a36f8a8e8a304e59abca52d378
whyoleg <whyoleg@gmail.com>
2019-11-15 01:18:35 +0300

    some fixes

--- api/generator/src/main/kotlin/dev/whyoleg/ktd/generator/JniGenerator.kt
+++ api/generator/src/main/kotlin/dev/whyoleg/ktd/generator/JniGenerator.kt
@@ -1,6 +1,7 @@
 package dev.whyoleg.ktd.generator
 
 import eu.jrie.jetbrains.kotlinshell.shell.*
+import kotlinx.coroutines.*
 import org.kohsuke.github.*
 import java.io.*
 
@@ -51,71 +52,75 @@ val target by lazy {
     val target = when {
         os.contains("linux")   -> Target.Linux
         os.contains("mac")     -> Target.Mac
-        os.contains("windows") -> if (System.getProperty("ARCH").toLowerCase().contains("32")) Target.Win32 else Target.Win64
+        os.contains("windows") -> if (System.getProperty("ARCH")?.toLowerCase()?.contains("32") == true) Target.Win32 else Target.Win64
         else                   -> error("Target is not supported")
     }
     println("Start build on $target")
     target
 }
 
-suspend fun main(vararg args: String) {
+fun main(vararg args: String) {
+    val apiVersion = args.first()
+    val commitSha = GitHub.connectAnonymously().findCommit(apiVersion).shA1
     System
         .getProperties()
         .toList()
-        .sortedBy { it.first.toString() }
+        .sortedBy { it.first?.toString() }
         .forEach { (key, value) -> println("$key: $value") }
-    target
-    val apiVersion = args.first()
-    val commitSha = GitHub.connectAnonymously().findCommit(apiVersion).shA1
-    println("Check chell")
-    shell(dir = File("td")) {
-        "echo 322"()
-        "git reset --hard $commitSha"()
-    }
+    runBlocking {
+        delay(500)
+        target
 
-    val generatedPath = "api/v$apiVersion/generated"
-    val buildDir = File("td/build-v$apiVersion")
-    val generatedDir = File(generatedPath)
-    val generatedBuildDir = generatedDir.resolve("build")
-
-    File("api/generator/jni").copyFilesTo(generatedDir, "td_jni.cpp", "CMakeLists.txt")
-
-    println("Build dir: ${buildDir.absolutePath}")
-    println("Generated dir: ${generatedDir.absolutePath}")
-    println("Generated build dir: ${generatedBuildDir.absolutePath}")
-
-    println("Build tdlib")
-    build(
-        buildDir,
-        "-DCMAKE_INSTALL_PREFIX:PATH=../../$generatedPath/td",
-        "-DTD_ENABLE_LTO=ON",
-        "-DTD_ENABLE_JNI=ON"
-    )
-    println("Generate tdlib")
-    build(
-        generatedBuildDir,
-        "-DTd_DIR=${generatedDir.absolutePath}/td/lib/cmake/Td",
-        "-DCMAKE_INSTALL_PREFIX:PATH=.."
-    )
-    //    delay(2000)
-    generatedDir.listFiles().orEmpty().forEach {
-        println(it.absolutePath)
-        if (!it.isFile) {
-            println("START DIRECTORY")
-            it.listFiles().orEmpty().forEach {
-                println(it.absolutePath)
-            }
-            println("END   DIRECTORY")
+        println("Check chell")
+        shell(dir = File("td")) {
+            "echo 322"()
+            "git reset --hard $commitSha"()
         }
+
+        val generatedPath = "api/v$apiVersion/generated"
+        val buildDir = File("td/build-v$apiVersion")
+        val generatedDir = File(generatedPath)
+        val generatedBuildDir = generatedDir.resolve("build")
+
+        File("api/generator/jni").copyFilesTo(generatedDir, "td_jni.cpp", "CMakeLists.txt")
+
+        println("Build dir: ${buildDir.absolutePath}")
+        println("Generated dir: ${generatedDir.absolutePath}")
+        println("Generated build dir: ${generatedBuildDir.absolutePath}")
+
+        println("Build tdlib")
+        build(
+            buildDir,
+            "-DCMAKE_INSTALL_PREFIX:PATH=../../$generatedPath/td",
+            "-DTD_ENABLE_LTO=ON",
+            "-DTD_ENABLE_JNI=ON"
+        )
+        println("Generate tdlib")
+        build(
+            generatedBuildDir,
+            "-DTd_DIR=${generatedDir.absolutePath}/td/lib/cmake/Td",
+            "-DCMAKE_INSTALL_PREFIX:PATH=.."
+        )
+        //    delay(2000)
+        generatedDir.listFiles().orEmpty().forEach {
+            println(it.absolutePath)
+            if (!it.isFile) {
+                println("START DIRECTORY")
+                it.listFiles().orEmpty().forEach {
+                    println(it.absolutePath)
+                }
+                println("END   DIRECTORY")
+            }
+        }
+        val size = generatedDir.resolve("bin/libtdjni.so").readBytes().size
+
+        println("Generated tdlib size: $size b")
+        println("Generated tdlib size: ${size / 1024} mb")
+
+        //    generatedDir
+        //        .resolve("bin")
+        //        .copyFilesTo(File("api/v$apiVersion/src/main/libs/linux/amd64"), "libtdjni.so")
     }
-    val size = generatedDir.resolve("bin/libtdjni.so").readBytes().size
-
-    println("Generated tdlib size: $size b")
-    println("Generated tdlib size: ${size / 1024} mb")
-
-    //    generatedDir
-    //        .resolve("bin")
-    //        .copyFilesTo(File("api/v$apiVersion/src/main/libs/linux/amd64"), "libtdjni.so")
 }
 
 fun File.copyFilesTo(path: File, vararg names: String) {

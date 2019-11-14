571f1688d9b746a81659b10298fc9aa98493149e
whyoleg <whyoleg@gmail.com>
2019-11-15 00:55:57 +0300

    setup build mainly macos

--- api/generator/src/main/kotlin/dev/whyoleg/ktd/generator/JniGenerator.kt
+++ api/generator/src/main/kotlin/dev/whyoleg/ktd/generator/JniGenerator.kt
@@ -1,27 +1,44 @@
 package dev.whyoleg.ktd.generator
 
 import eu.jrie.jetbrains.kotlinshell.shell.*
-import kotlinx.coroutines.*
 import org.kohsuke.github.*
 import java.io.*
 
 const val DCMAKE_BUILD_TYPE = "MinSizeRel"
 val env = mapOf("CC" to "/usr/bin/clang-6.0", "CXX" to "/usr/bin/clang++-6.0")
 
-suspend fun Shell.cmake(vararg commands: String) =
-    ("cmake " +
-            "-DCMAKE_BUILD_TYPE=$DCMAKE_BUILD_TYPE " +
-            "-DCMAKE_AR=/usr/bin/llvm-ar-6.0 " +
-            "-DCMAKE_NM=/usr/bin/llvm-nm-6.0 " +
-            "-DCMAKE_OBJDUMP=/usr/bin/llvm-objdump-6.0 " +
-            "-DCMAKE_RANLIB=/usr/bin/llvm-ranlib-6.0 " +
-            commands.joinToString(" ") +
-            " ..").invoke()
+suspend fun Shell.cmake(vararg commands: String) {
+    val cmd = when (target) {
+        Target.Linux -> listOf(
+            "-DCMAKE_BUILD_TYPE=$DCMAKE_BUILD_TYPE",
+            "-DCMAKE_AR=/usr/bin/llvm-ar-6.0",
+            "-DCMAKE_NM=/usr/bin/llvm-nm-6.0",
+            "-DCMAKE_OBJDUMP=/usr/bin/llvm-objdump-6.0",
+            "-DCMAKE_RANLIB=/usr/bin/llvm-ranlib-6.0"
+        )
+        Target.Mac   -> listOf(
+            "-DCMAKE_BUILD_TYPE=$DCMAKE_BUILD_TYPE",
+            "-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/"
+        )
+        else         -> listOf(
+            if (target == Target.Win32) "-A Win32" else "-A Win64",
+            "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=../vcpkg/scripts/buildsystems/vcpkg.cmake"
+        )
+    }
 
-suspend fun Shell.install() = "cmake --build . --target install".invoke()
+    ("cmake ${cmd.joinToString(" ")} ${commands.joinToString(" ")} ..").invoke()
+}
+
+suspend fun Shell.install() {
+    val addition = if (target == Target.Win32 || target == Target.Win64) " --config $DCMAKE_BUILD_TYPE" else ""
+    "cmake --build . --target install$addition".invoke()
+}
 
 suspend fun build(dir: File, vararg commands: String) {
-    shell(dir = dir, env = env) {
+    dir.deleteRecursively()
+    dir.mkdirs()
+    if (target == Target.Win32 || target == Target.Win64) shell { "Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue"() }
+    shell(dir = dir, env = if (target == Target.Linux) env else null) {
         cmake(*commands)
         install()
     }
@@ -34,8 +51,7 @@ val target by lazy {
     val target = when {
         os.contains("linux")   -> Target.Linux
         os.contains("mac")     -> Target.Mac
-        os.contains("windows") -> if (System.getProperty("WINDOWS_ARCH").toLowerCase().contains("32")) Target.Win32
-        else Target.Win64
+        os.contains("windows") -> if (System.getProperty("ARCH").toLowerCase().contains("32")) Target.Win32 else Target.Win64
         else                   -> error("Target is not supported")
     }
     println("Start build on $target")
@@ -43,17 +59,20 @@ val target by lazy {
 }
 
 suspend fun main(vararg args: String) {
+    println(System.getProperty("ARCH"))
     target
     val apiVersion = args.firstOrNull() ?: "1.5.0"
     val commitSha = GitHub.connectAnonymously().findCommit(apiVersion).shA1
-    shell(dir = File("td")) { "git reset --hard $commitSha"() }
+    println("Check chell")
+    shell(dir = File("td")) {
+        "echo 322"()
+        "git reset --hard $commitSha"()
+    }
 
     val generatedPath = "api/v$apiVersion/generated"
     val buildDir = File("td/build-v$apiVersion")
     val generatedDir = File(generatedPath)
     val generatedBuildDir = generatedDir.resolve("build")
-    buildDir.mkdirs()
-    generatedBuildDir.mkdirs()
 
     File("api/generator/jni").copyFilesTo(generatedDir, "td_jni.cpp", "CMakeLists.txt")
 
@@ -74,14 +93,15 @@ suspend fun main(vararg args: String) {
         "-DTd_DIR=${generatedDir.absolutePath}/td/lib/cmake/Td",
         "-DCMAKE_INSTALL_PREFIX:PATH=.."
     )
-    delay(2000)
+    //    delay(2000)
     generatedDir.listFiles().orEmpty().forEach {
         println(it.absolutePath)
         if (!it.isFile) {
-            println("DIRECTORY")
+            println("START DIRECTORY")
             it.listFiles().orEmpty().forEach {
                 println(it.absolutePath)
             }
+            println("END   DIRECTORY")
         }
     }
     val size = generatedDir.resolve("bin/libtdjni.so").readBytes().size

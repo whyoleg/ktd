106c535d67fa0282b2e75e2956774236be773dd9
whyoleg <whyoleg@gmail.com>
2019-11-15 12:19:35 +0300

    fix win build

--- api/generator/src/main/kotlin/dev/whyoleg/ktd/generator/JniGenerator.kt
+++ api/generator/src/main/kotlin/dev/whyoleg/ktd/generator/JniGenerator.kt
@@ -24,7 +24,7 @@ suspend fun Shell.cmake(vararg commands: String) {
         else         -> listOf(
             if (target == Target.Win32) "-A Win32" else "-A x64",
             "-DCMAKE_TOOLCHAIN_FILE:FILEPATH=../../vcpkg/scripts/buildsystems/vcpkg.cmake",
-            "-DGPERF_EXECUTABLE=../../gperf-win/bin"
+            "-DGPERF_EXECUTABLE=../../gperf-win/bin/gperf.exe"
         )
     }
 

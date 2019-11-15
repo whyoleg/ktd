f93f43148f44515560667754703b7eb33359e399
whyoleg <whyoleg@gmail.com>
2019-11-15 23:18:40 +0000

    New api 1.5.0

---
+++ api/v1.5.0/src/main/kotlin/dev/whyoleg/ktd/api/Experimental.kt
@@ -0,0 +1,8 @@
+package dev.whyoleg.ktd.api
+
+@Experimental(level = Experimental.Level.WARNING)
+annotation class BotsOnly
+
+@Experimental(level = Experimental.Level.WARNING)
+annotation class TestingOnly
+

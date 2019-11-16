9a632fe80eb19c6dae8971fb3a22cf2430812fa8
whyoleg <whyoleg@gmail.com>
2019-11-16 00:07:53 +0000

    New api 1.5.0

---
+++ api/v1.5.0/src/main/kotlin/dev/whyoleg/ktd/api/emoji/Parameterized.kt
@@ -0,0 +1,58 @@
+@file:Suppress(
+    "unused"
+)
+@file:UseExperimental(
+    BotsOnly::class,
+    TestingOnly::class
+)
+
+package dev.whyoleg.ktd.api.emoji
+
+import dev.whyoleg.ktd.*
+import dev.whyoleg.ktd.api.*
+import dev.whyoleg.ktd.api.TdApi.*
+
+/**
+ * Returns emoji corresponding to a sticker
+ * The list is only for informational purposes, because a sticker is always sent with a fixed emoji from the corresponding Sticker object
+ *
+ * @sticker - Sticker file identifier
+ */
+suspend fun TelegramClient.getStickerEmojis(
+    sticker: InputFile
+): Emojis = emoji(
+    GetStickerEmojis(
+        sticker
+    )
+)
+
+/**
+ * Searches for emojis by keywords
+ * Supported only if the file database is enabled
+ *
+ * @text - Text to search for
+ * @exactMatch - True, if only emojis, which exactly match text needs to be returned
+ */
+suspend fun TelegramClient.searchEmojis(
+    text: String,
+    exactMatch: Boolean = false
+): Emojis = emoji(
+    SearchEmojis(
+        text,
+        exactMatch
+    )
+)
+
+/**
+ * Returns an HTTP URL which can be used to automatically log in to the translation platform and suggest new emoji replacements
+ * The URL will be valid for 30 seconds after generation
+ *
+ * @languageCode - Language code for which the emoji replacements will be suggested
+ */
+suspend fun TelegramClient.getEmojiSuggestionsUrl(
+    languageCode: String
+): HttpUrl = emoji(
+    GetEmojiSuggestionsUrl(
+        languageCode
+    )
+)
---
+++ api/v1.5.0/src/main/kotlin/dev/whyoleg/ktd/api/shipping/Parameterized.kt
@@ -0,0 +1,33 @@
+@file:Suppress(
+    "unused"
+)
+@file:UseExperimental(
+    BotsOnly::class,
+    TestingOnly::class
+)
+
+package dev.whyoleg.ktd.api.shipping
+
+import dev.whyoleg.ktd.*
+import dev.whyoleg.ktd.api.*
+import dev.whyoleg.ktd.api.TdApi.*
+
+/**
+ * Sets the result of a shipping query
+ *
+ * @shippingQueryId - Identifier of the shipping query
+ * @shippingOptions - Available shipping options
+ * @errorMessage - An error message, empty on success
+ */
+@BotsOnly
+suspend fun TelegramClient.answerShippingQuery(
+    shippingQueryId: Long = 0L,
+    shippingOptions: Array<ShippingOption> = emptyArray(),
+    errorMessage: String
+): Ok = shipping(
+    AnswerShippingQuery(
+        shippingQueryId,
+        shippingOptions,
+        errorMessage
+    )
+)

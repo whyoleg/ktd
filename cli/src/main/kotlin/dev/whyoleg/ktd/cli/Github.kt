2ec689b02ae92fdddcb7fa9444ffd19edf630ebb
whyoleg <whyoleg@gmail.com>
2020-01-19 22:34:17 +0300

    update refs

--- cli/src/main/kotlin/dev/whyoleg/ktd/cli/Github.kt
+++ cli/src/main/kotlin/dev/whyoleg/ktd/cli/Github.kt
@@ -7,10 +7,9 @@ import java.io.*
 val tdVersionRefs = mapOf(
     "1.5.0" to "e2734d4fb6815da5f357da4731d3013f64410e6b",
     "1.5.1" to "9b0530e704158e56384e9b692590a9227e0cba83",
-    "1.5.2" to "0d0159bc42bf9eb13057a68e983146973b45f3df",
-    "1.5.3" to "917a3af6acda14f6cecafa9e3fbaaa134675cef5",
+    "1.5.2" to "ab534a9e773b950dd5fc7142d9862b2f0067862b",
+    "1.5.3" to "6bfb4a79f29cb55d14a42e1dc667a019b0056b9c",
     "1.5.4" to "b7928e64a68194d97011e4f3f015be65a0fbe8ca"
-    //    "1.6.0" to "92c9149e30318ac83baeb194ba9fcacf2bea3588" //latest commit from master branch
 )
 
 val tdVersions = tdVersionRefs.keys.sorted()
